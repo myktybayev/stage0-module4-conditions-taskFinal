41dc9171ac6aa2ff6c6f5f8c360542d860bca562
Mykty <baha@Bahas-MacBook-Pro.local>
2022-08-12 12:31:26 +0600

    final

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -3,5 +3,37 @@ package school.mjc.stage0.conditions.finalTask;
 public class DaysInMonth {
     public void printDays(int year, int month) {
 
+        int amountOfDays = 0;
+        switch (month) {
+            case 2:
+                if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
+                    amountOfDays = 29;
+                } else {
+                    amountOfDays = 28;
+                }
+
+                System.out.println(amountOfDays);
+
+                break;
+
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                System.out.println("31");
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                System.out.println("30");
+                break;
+            default:
+                System.out.println("invalid date");
+                break;
+        }
     }
 }
